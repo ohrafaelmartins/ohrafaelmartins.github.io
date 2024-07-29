0607f69e108ea2def596a01058fcff4092bc1b97
Rafael Martins <rafael@arvore.com.br>
2024-07-29 08:53:13 -0300

    Atualizar site de vendas - Décima versao - ebook guia de viagem de carro ao norte da argentina

--- facebook-pixel.js
+++ facebook-pixel.js
@@ -1,22 +1,4 @@
-// facebook-pixel.js
-!function (f, b, e, v, n, t, s) {
-    if (f.fbq) return; 
-    n = f.fbq = function () {
-        n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-    };
-    if (!f._fbq) f._fbq = n; 
-    n.push = n; 
-    n.loaded = !0; 
-    n.version = '2.0';
-    n.queue = []; 
-    t = b.createElement(e); 
-    t.async = !0;
-    t.src = v; 
-    s = b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t, s)
-}(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', 'YOUR_PIXEL_ID'); // Substitua YOUR_PIXEL_ID pelo ID do seu Pixel
-fbq('track', 'PageView');
+
 
 // Função para monitorar a exibição da seção
 function trackCheckoutView() {
@@ -24,8 +6,8 @@ function trackCheckoutView() {
     var hasTracked = false;
 
     // Criação de um observer para monitorar a visibilidade da seção
-    var observer = new IntersectionObserver(function(entries) {
-        entries.forEach(function(entry) {
+    var observer = new IntersectionObserver(function (entries) {
+        entries.forEach(function (entry) {
             if (entry.isIntersecting && !hasTracked) {
                 fbq('track', 'ViewContent', {
                     content_name: 'Checkout Section',
@@ -44,6 +26,6 @@ function trackCheckoutView() {
 }
 
 // Chamar a função para iniciar o rastreamento quando o DOM estiver pronto
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     trackCheckoutView();
 });
